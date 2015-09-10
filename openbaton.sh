ca639b5e809eecde21eda235236a4336104d3a02
tiziartas <cecamoretiziano25@gmail.com>
2015-09-10 12:01:04 +0200

    new script launcher with usage

--- openbaton.sh
+++ openbaton.sh
@@ -20,6 +20,16 @@ function check_not_running {
         fi
 }
 
+function usage {
+    echo -e "Open-Baton\n"
+    echo -e "Usage:\n\t ./openbaton.sh [option] comand [comand] [comand]\n\t"
+    echo -e "where option is"
+    echo -e "\t\t * -c path of properties file"
+    echo -e "\t\t * -d debug mode"
+    echo -e "comand help to see the list of the comands"
+    echo -e "./openbaton.sh help"
+}
+
 ##
 #   MAIN
 ##
@@ -41,10 +51,8 @@ shift $(( OPTIND - 1 ))
 
 if [ $# -eq 0 ]
 then
-        check_already_running_client
-        check_not_running
-        cd cli/build/libs/
-        java -jar "cli-all-$_version.jar" "help"
+        usage
+        exit 1
 else
         check_already_running_client
         check_not_running
