77095cd4bfcbfe0351b7d7798c160f27dce8d614
Oleksander Gozman <oleksander.gozman@fokus.fraunhofer.de>
2016-03-08 11:29:03 +0100

    the base of packageagent, not finished

--- sdk/src/main/java/org/openbaton/sdk/api/rest/VNFPackageAgent.java
+++ sdk/src/main/java/org/openbaton/sdk/api/rest/VNFPackageAgent.java
@@ -1,30 +1,28 @@
 package org.openbaton.sdk.api.rest;
-
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.openbaton.catalogue.nfvo.NFVImage;
-import org.openbaton.catalogue.nfvo.Script;
 import org.openbaton.catalogue.nfvo.VNFPackage;
-import org.openbaton.sdk.api.annotations.Help;
 import org.openbaton.sdk.api.util.AbstractRestAgent;
 
-import java.io.File;
-import java.io.IOException;
+
+import java.io.*;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 
 /**
@@ -35,117 +33,7 @@ public class VNFPackageAgent extends AbstractRestAgent<VNFPackage> {
     public VNFPackageAgent(String username, String password, String nfvoIp, String nfvoPort, String path, String version) {
         super(username, password, nfvoIp, nfvoPort, path, version, VNFPackage.class);
     }
-
-    /**
-     * Return the list of all VNFPackages as VNFPackage objects
-     *
-     *
-     * @return : The List of
-     * VNFPackage uploaded to the NFVO database
-     */
-
-    @Help(help = "Retrieve all the packages as a list of VNFPackage objects")
-    public List<VNFPackage> findAll() {
-        CloseableHttpClient client = HttpClients.createDefault();
-        HttpGet get = new HttpGet(baseUrl);
-        CloseableHttpResponse response = null;
-        try {
-            response = client.execute(get);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        String respS = null;
-        try {
-            respS = EntityUtils.toString(response.getEntity());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        //System.out.println(respS);
-        JSONArray array = new JSONArray(respS);
-
-        List<VNFPackage> packages = new ArrayList<>();
-        for (int i = 0; i < array.length(); i++) {
-            VNFPackage newPackage = new VNFPackage();
-            JSONObject elem = array.getJSONObject(i);
-            newPackage.setId(elem.getString("id"));
-            try {
-                newPackage.setImageLink(elem.getString("imageLink"));
-            }
-            catch (Exception e) {
-                e.printStackTrace();
-            }
-            newPackage.setName(elem.getString("name"));
-            newPackage.setVersion(elem.getInt("version"));
-            NFVImage newImage = new NFVImage();
-            newImage.setId(elem.getJSONObject("image").getString("id"));
-            newImage.setVersion(elem.getJSONObject("image").getInt("version"));
-            newImage.setMinRam(elem.getJSONObject("image").getInt("minRam"));
-            newImage.setMinDiskSpace(elem.getJSONObject("image").getInt("minDiskSpace"));
-            newImage.setIsPublic(elem.getJSONObject("image").getBoolean("isPublic"));
-            newPackage.setImage(newImage);
-            try {
-                newPackage.setScripts(listScripts(elem));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            try {
-                newPackage.setScriptsLink(elem.getString("scriptsLink"));
-            }
-            catch (Exception e) {
-                newPackage.setScriptsLink(null);
-            }
-            packages.add(newPackage);
-
-        }
-        System.out.println(packages.toString());
-
-        return packages;
-
-
-    }
-
-    /**
-     * Upload a package to the NFVO via REST api
-     *
-     * @param file : Path to the package file
-     * @return NFVO server answer as String
-     *
-     */
-    @Help(help = "Upload a package to NFVO")
-    public String create(String file) throws Exception {
-        CloseableHttpClient client = HttpClients.createDefault();
-        HttpPost post = new HttpPost(this.baseUrl);
-        MultipartEntityBuilder builder = MultipartEntityBuilder.create();
-        builder.addBinaryBody
-                ("file", new File(file), ContentType.APPLICATION_OCTET_STREAM, "file.ext");
-        HttpEntity entity = builder.build();
-        post.setEntity(entity);
-        HttpResponse response = null;
-        try {
-            response = client.execute(post);
-            System.out.println(response.toString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return response.toString();
-    }
-    /**
-     * Delete the package with a specific id
-     *
-     * @param id : the id of the package to be deleted
-     *
-     */
-    @Help(help = "Delete package with a specific id")
-    public void delete(String id) {
-        CloseableHttpClient client = HttpClients.createDefault();
-        HttpDelete delete = new HttpDelete(baseUrl + "/" + id);
-        try {
-            client.execute(delete);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    private static byte[]  scripttoString(JSONArray payload) {
+    private static String scripttoString(JSONArray payload) {
         String script = null;
         ArrayList<String> list = new ArrayList<String>();
         if (payload != null) {
@@ -157,45 +45,30 @@ public class VNFPackageAgent extends AbstractRestAgent<VNFPackage> {
         String[] arrayS = new String[list.size()];
         arrayS = list.toArray(arrayS);
         byte[] b = new byte[arrayS.length];
+        //System.out.println(arrayS[1]);
         for (int k = 0; k < arrayS.length - 1; k++) {
             int l = Integer.parseInt(arrayS[k]);
 
             b[k] = (byte)l;
 
         }
-        return b;
-    }
-    private static Set<Script> listScripts(JSONObject obj) throws Exception {
-        Set<Script> scriptsList = new HashSet<>();
-        //JSONArray array = new JSONArray(respS);
 
-        //System.out.println("Starting conversion");
-        //for (int i = 0; i < array.length(); i++) {
+        try {
+            script = new String(b, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
 
-            JSONArray scripts = obj.getJSONArray("scripts");
-            for (int j = 0; j < scripts.length(); j++) {
-                Script elem = new Script();
-                elem.setId(scripts.getJSONObject(j).getString("id"));
-                elem.setVersion(scripts.getJSONObject(j).getInt("version"));
-                elem.setName(scripts.getJSONObject(j).getString("name"));
-                JSONArray payload = scripts.getJSONObject(j).getJSONArray("payload");
-                elem.setPayload(scripttoString(payload));
-                scriptsList.add(elem);
-            }
-        //}
-        return scriptsList;
+
+
+        return script;
     }
 
-    /**
-     * Retrieves the package with a specific id
-     *
-     * @param id : the id of the package to be retrieved
-     *
-     */
-    public VNFPackage find(String id) {
-        VNFPackage newPackage = new VNFPackage();
+
+    public List<VNFPackage> findAll() {
+        System.out.println("Accessing Packages");
         CloseableHttpClient client = HttpClients.createDefault();
-        HttpGet get = new HttpGet(baseUrl + "/" + id);
+        HttpGet get = new HttpGet("http://localhost:8080/api/v1/vnf-packages");
         CloseableHttpResponse response = null;
         try {
             response = client.execute(get);
@@ -208,54 +81,96 @@ public class VNFPackageAgent extends AbstractRestAgent<VNFPackage> {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        JSONObject elem = null;
-        try {
-            elem = new JSONObject(respS);
-        } catch(org.json.JSONException e) {
-            return null;
-        }
+        //System.out.println("Fetching scripts");
         //System.out.println(respS);
-        newPackage.setId(elem.getString("id"));
-        try {
-            newPackage.setImageLink(elem.getString("imageLink"));
-        }
-        catch (Exception e) {
+        JSONArray array = new JSONArray(respS);
+        //System.out.println(listScripts(respS));
+        List<VNFPackage> packages = new ArrayList<>();
+        for (int i = 0; i < array.length(); i++) {
+            VNFPackage newPackage = new VNFPackage();
+            JSONObject elem = array.getJSONObject(i);
+            newPackage.setId(elem.getString("id"));
             newPackage.setImageLink(null);
+            newPackage.setName(elem.getString("name"));
+            newPackage.setVersion(elem.getInt("version"));
+            NFVImage newImage = new NFVImage();
+            newImage.setId(elem.getJSONObject("image").getString("id"));
+            newImage.setVersion(elem.getJSONObject("image").getInt("version"));
+            newImage.setMinRam(elem.getJSONObject("image").getInt("minRam"));
+            newImage.setMinDiskSpace(elem.getJSONObject("image").getInt("minDiskSpace"));
+            newImage.setIsPublic(elem.getJSONObject("image").getBoolean("isPublic"));
+            newPackage.setImage(newImage);
+            try {
+                newPackage.setScriptsLink(elem.getString("scriptsLink"));
+            }
+            catch (Exception e) {
+                newPackage.setImageLink(null);
+            }
+            packages.add(newPackage);
+
+        }
+        System.out.println(packages.toString());
+        return packages;
+
+
+    }
+
+
+    private static String listScripts(String respS) throws Exception {
+
+        String scriptsString = "";
+        JSONArray array = new JSONArray(respS);
+
+        System.out.println("Starting conversion");
+        for (int i = 0; i < array.length(); i++) {
+            JSONObject obj = array.getJSONObject(i);
+            JSONArray scripts = obj.getJSONArray("scripts");
+            for (int j = 0; j < scripts.length(); j++) {
+                JSONArray payload = scripts.getJSONObject(j).getJSONArray("payload");
+                String final1 = scripttoString(payload);
+                scriptsString = scriptsString.concat(final1);
+                scriptsString = scriptsString.concat("\n\n\n\n");
+            }
+
+
+        }
+        System.out.println("Done");
+        return scriptsString;
+    }
+    public static void create(String file) throws Exception {
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost post = new HttpPost("http://localhost:8080/api/v1/vnf-packages");
+        MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+        builder.addBinaryBody
+                ("file", new File(file), ContentType.APPLICATION_OCTET_STREAM, "file.ext");
+        HttpEntity entity = builder.build();
+        post.setEntity(entity);
+        try {
+            HttpResponse response = client.execute(post);
+            System.out.println(response.toString());
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        newPackage.setName(elem.getString("name"));
-        newPackage.setVersion(elem.getInt("version"));
-        NFVImage newImage = new NFVImage();
-        newImage.setId(elem.getJSONObject("image").getString("id"));
-        newImage.setVersion(elem.getJSONObject("image").getInt("version"));
-        newImage.setMinRam(elem.getJSONObject("image").getInt("minRam"));
-        newImage.setMinDiskSpace(elem.getJSONObject("image").getInt("minDiskSpace"));
-        newImage.setIsPublic(elem.getJSONObject("image").getBoolean("isPublic"));
-        newPackage.setImage(newImage);
+    }
+
+    public static void main(String[] arg) throws IOException {
         try {
-            newPackage.setScripts(listScripts(elem));
+            //findAll();
         } catch (Exception e) {
             e.printStackTrace();
         }
         try {
-            newPackage.setScriptsLink(elem.getString("scriptsLink"));
-        }
-        catch (Exception e) {
-            newPackage.setScriptsLink(null);
+            //postPackage("/home/packages/scscf.tar");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-
-        return newPackage;
 
     }
 
 
 
-}
-
-
-
-
+    }
 
 
 
